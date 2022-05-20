6e3c13dce54e729e7532eed833c0c3e5161df4f3
user <user@DESKTOP-8K8IAI3>
2022-05-20 17:23:18 +0900

    윤

--- abnext/src/main/java/kr/or/abnext/login/LoginController.java
+++ abnext/src/main/java/kr/or/abnext/login/LoginController.java
@@ -1,10 +1,28 @@
 package kr.or.abnext.login;
 
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class LoginController {
 	@Autowired
 	LoginService loginServ;
+	
+	
+	/*
+	 * 로그인페이지 연결.
+	 * */
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String home(Model model) {
+		
+		return "login/login";
+	}
+	
 }
